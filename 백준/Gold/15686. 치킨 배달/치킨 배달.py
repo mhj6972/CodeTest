1bcd9138c9ca8d9ad5036cbcf11dde678e136d16
mhj6972 <48512290+mhj6972@users.noreply.github.com>
2022-08-04 15:20:56 +0900

    [Gold V] Title: 치킨 배달, Time: 400 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -1,28 +1,27 @@
+from itertools import *
 import sys
-from itertools import combinations
 
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-city = list(list(map(int, input().split())) for _ in range(n))
-result = 999999
-house = []      # 집의 좌표
-chick = []      # 치킨집의 좌표
-
+answer = []
 for i in range(n):
-    for j in range(n):
-        if city[i][j] == 1:
-            house.append([i, j])
-        elif city[i][j] == 2:
-            chick.append([i, j])
+  answer.append(list(map(int, input().split())))
 
-for chi in combinations(chick, m):  # m개의 치킨집 선택
-    temp = 0            # 도시의 치킨 거리
-    for h in house: 
-        chi_len = 999   # 각 집마다 치킨 거리
-        for j in range(m):
-            chi_len = min(chi_len, abs(h[0] - chi[j][0]) + abs(h[1] - chi[j][1]))
-        temp += chi_len
-    result = min(result, temp)
+ch = []
+home = []
+for i in range(n):
+  for j in range(len(answer[0])):
+    if answer[i][j] == 1: home.append([i,j])
+    elif answer[i][j] == 2: ch.append([i,j])
 
-print(result)
+minv = 100000
+for chicken in combinations(ch,m):
+  sumv = 0
+  for j in home:
+    sumv += min([abs(j[0]-k[0])+abs(j[1]-k[1]) for k in chicken])
+    if minv <= sumv: break
+  if sumv < minv:
+    minv = sumv
+
+print(minv)
